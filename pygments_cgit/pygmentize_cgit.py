e8441065da9d68957e9abc2ee46a4358d0c202d8
Sebastien Martini <seb@dbzteam.org>
2011-12-21 13:47:29 +0100

    Fixed pygmentization's encoding.

--- pygments_cgit/pygmentize_cgit.py
+++ pygments_cgit/pygmentize_cgit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# 12/18/2009 - seb@dbzteam.org
+# 12/21/2011 - seb@dbzteam.org
 #
 # Use Pygments (http://pygments.org/) to highlight files managed with git
 # and web interfaced with cgit (http://hjemli.net/git/cgit/).
@@ -9,26 +9,25 @@
 #
 # 1- Install python-pygments and python-chardet
 # 2- Copy this script to /usr/local/bin/pygmentize_cgit.py (with exec rights)
-# 3- Copy 'pygments.css' and 'head' into /var/www/<git_dir>/
+# 3- Copy 'pygments.css' (or edit CSS_FILE below) and 'head' into /var/www/cgit/
 # 4- Add these statements under 'global settings' section of cgit
 #    configuration file /etc/cgitrc:
 #      # Include the content of this file verbatim in HEAD
-#      head-include=/var/www/<git_dir>/head
+#      head-include=/var/www/cgit/head
 #      # Source code highlighting
 #      source-filter=/usr/local/bin/pygmentize_cgit.py
-# 5- Edit pygmentize_cgit.py and modify the variable CSS_FILE with
-#    '/var/www/<git_dir>/pygments.css'
 #
 import sys
+
 import chardet
 import pygments
 import pygments.lexers
 import pygments.formatters
 
-# CSS FILE's location
-CSS_FILE = '/var/www/git/pygments.css'
+CSS_FILE = '/var/www/cgit/pygments.css'
 
-def pygmentize(fn, in_stream=None, out_stream=None, debug=False):
+
+def pygmentize(fn, in_stream=None, out_stream=None):
     # If not provided in_stream will be read from stdin and out_stream
     # will be written to stdout.
     if in_stream is None:
@@ -36,48 +35,42 @@ def pygmentize(fn, in_stream=None, out_stream=None, debug=False):
     if out_stream is None:
         out_stream = sys.stdout
 
-    # Use pygments to highlight in_stream.
-    highlight = True
-
     lexer = None
     try:
         lexer = pygments.lexers.get_lexer_for_filename(fn, encoding='chardet')
     except pygments.util.ClassNotFound:
-        lexer = pygments.lexers.TextLexer()
-
-    formatter = None
-    try:
-        formatter = pygments.formatters.get_formatter_by_name('html',
-                                                              cssfile=CSS_FILE,
-                                                              noclobber_cssfile=True,
-                                                              style='default')
-    except pygments.util.ClassNotFound:
-        highlight = False
+        lexer = pygments.lexers.TextLexer(encoding='chardet')
 
     # Read input stream.
     in_data = in_stream.read()
+
     # Detect input charset.
-    encoding = chardet.detect(in_data)
-
-    if debug:
-        # print detected encoding
-        k = file('/tmp/encoding', 'w')
-        try:
-            k.write(str(encoding) + '\n')
-        finally:
-            k.close()
-
-    if (highlight and ('encoding' in encoding) and
-        (encoding['encoding'] in ('utf-8', 'ascii'))):
-        # Sadly we only pygment iff we are sure that the underlying encoding
-        # won't be a problem i.e. currently only utf8 and ascii.
-        ret = pygments.highlight(in_data, lexer, formatter, outfile=out_stream)
-    else:
-        # If sth went wrong or the file extension is no recognized or in doubt
-        # copy in_stream to out_stream without any modifications.
+    chardet_encoding = chardet.detect(in_data)
+    if 'encoding' not in chardet_encoding:
         out_stream.write(in_data)
+        return
+
+    # DEBUG: output detected encoding
+    #k = file('/tmp/encoding', 'w')
+    #k.write(str(chardet_encoding) + '\n')
+    #k.close()
+
+    formatter = None
+    try:
+        outencoding = chardet_encoding['encoding']
+        formatter = pygments.formatters.get_formatter_by_name('html',
+                                                              cssfile=CSS_FILE,
+                                                              outencoding=outencoding,
+                                                              noclobber_cssfile=True,
+                                                              style='default')
+    except pygments.util.ClassNotFound:
+        out_stream.write(in_data)
+        return            
+
+    pygments.highlight(in_data, lexer, formatter, outfile=out_stream)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit(1)
-    pygmentize(sys.argv[1], debug=False)
+    pygmentize(sys.argv[1])
